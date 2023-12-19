a970d8cc85f24946c9a2c7d898d814d251c4af76
NABIN720 <nabinbhattarai720.com>
2023-12-19 07:51:58 +0545

    changed liner with ex

--- linearregression.py
+++ linearregression.py
@@ -6,12 +6,19 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 
-x = [5,7,8,4,6,9,3,10,11,12,13,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = [5, 7, 8, 4, 6, 9, 3, 10, 11, 12, 13, 9, 6]
+y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
 
 slope, intercept, r, p, std_err = stats.linregress(x,y)
+print(slope)
+print(intercept)
+print(r)
+print(p)
+print(std_err)
+
 
 def myfunc(x):
+
     return slope*x + intercept
 
 mymodel = list(map(myfunc,x))
